6e38a65f38597691e516616928701c4b9e8e4083
Justinas Brazauskas <braz.justinas@gmail.com>
2024-02-27 07:05:24 +0000

    add feedbackform.jsx

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FeedbackItem() {
+  return (
+    <div className='card'>
+        <div className="num-display">10</div>
+        <div className="text-display">1This is an example of feedback</div>
+
+    </div>
+
+  )
+}
+
+export default FeedbackItem;
